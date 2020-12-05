42447a61c243cbd9ba9322905a87ed2e5b72ad22
bort <b.terentiev@gmail.com>
2020-12-05 16:58:47 +0100

    Removed google analytics as the current plugin doesn't work

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,8 +2,7 @@ module.exports = {
   title: 'bort\'s blog',
   description: 'A blog powered by VuePress',
   plugins: {
-    'disqus': { shortname: 'bortblog' },
-    '@vuepress/google-analytics': {'ga': 'G-N5BYM2SJXT'}
+    'disqus': { shortname: 'bortblog' }
   },
   theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
   themeConfig: {
